b8eeb329a16df4fde994a6f329e1144bbbfa2dc1
Jug4l <97116496+Jug4l@users.noreply.github.com>
2022-01-12 21:15:38 +0530

    Rename BEG_11.sql to BEG_4.sql

--- Beginner level queries/BEG_4.sql
+++ Beginner level queries/BEG_4.sql
@@ -1,6 +1,6 @@
 --11. In the output of the query above, showing the
 --Employees in order of BirthDate, we see the time of
---the BirthDate field, which we don’t want. Show only
+--the BirthDate field, which we donâ€™t want. Show only
 --the date portion of the BirthDate field.
 
 SELECT
